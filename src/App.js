1fe47d46641bce8252e8abdffbfe06d46f074a3a
tipb47 <tipb47@gmail.com>
2024-02-16 19:56:01 -0500

    fixed login/register methods.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 
 function App() {
   return (
     <div>
-      <Login />
+      <Register />
       <img src="./src/components/assets/logo.png" alt="rhythm reserve logo" />
     </div>  
   );
