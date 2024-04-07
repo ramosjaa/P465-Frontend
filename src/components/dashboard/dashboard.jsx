fa941687b4044bb34263778be057c1fcea21a526
bdokala <bdokala@iu.edu>
2024-04-07 17:40:05 -0400

    Adding search by name and location to the venue dashboard page

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -10,6 +10,7 @@ const Dashboard = () => {
     const navigate = useNavigate();
     const [userData, setUserData] = useState(null); // Add state for storing user data
     const [events, setEvents] = useState([]);
+    const [searched, setSearched] = useState(false);
 
     useEffect(() => {
         document.title = 'Dashboard | RhythmReserve';
@@ -20,44 +21,55 @@ const Dashboard = () => {
         <div>
             <div className="home-page text-white fw-bold">
                 <div className="dashboard">
-                    <h1>Welcome your dashboard!</h1>
+                    <h1>Welcome to your dashboard!</h1>
                     <p>You are now logged in! See venues below!</p>
-                    <Search setEvents={setEvents}/>
+                    <Search setEvents={setEvents} setSearched={setSearched}/>
 
-                    {/* Grid of cards */}
-                    {events == [] && <div className="row mt-5">
-                        {[...Array(9)].map((_, index) => (
-                            <div key={index} className="col-lg-3 col-md-4 col-sm-6 mb-4">
-                                <div className="card">
-                                    <img
-                                        src="https://resources.finalsite.net/images/f_auto,q_auto,t_image_size_1/v1682714955/brooksschoolorg/aucb7xbkp18dwu78z7mw/cabaret23-thumb.jpg"
-                                        className="card-img-top" alt="Placeholder"/>
-                                    <div className="card-body">
-                                        <h5 className="card-title">Venue</h5>
-                                        <p className="card-text">Click to see concerts</p>
+                    {/* Check if a search has been performed */}
+                    {searched ? (
+                        // If a search has been performed, only show this conditional based on events.length
+                        events.length > 0 ? (
+                            <Row xs={1} md={3} className="g-4">
+                                {events.map((event, index) => (
+                                    <Col key={index}>
+                                        <Card>
+                                            <Card.Img variant="top" src={event.fields.event_image_url}/>
+                                            <Card.Body>
+                                                <Card.Title>{event.fields.event_name}</Card.Title>
+                                                <Card.Text>
+                                                    <p>Location: {event.fields.event_location}</p>
+                                                    <p>Description: {event.fields.description}</p>
+                                                    {/* Add more event details as needed */}
+                                                </Card.Text>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                ))}
+                            </Row>
+                        ) : (
+                            // No results found after search
+                            <div className="row mt-5">
+                                <p>No related content found</p>
+                            </div>
+                        )
+                    ) : (
+                        // Default state, where no search has been performed yet, so show default cards
+                        <div className="row mt-5">
+                            {[...Array(9)].map((_, index) => (
+                                <div key={index} className="col-lg-3 col-md-4 col-sm-6 mb-4">
+                                    <div className="card">
+                                        <img
+                                            src="https://resources.finalsite.net/images/f_auto,q_auto,t_image_size_1/v1682714955/brooksschoolorg/aucb7xbkp18dwu78z7mw/cabaret23-thumb.jpg"
+                                            className="card-img-top" alt="Placeholder"/>
+                                        <div className="card-body">
+                                            <h5 className="card-title">Venue</h5>
+                                            <p className="card-text">Click to see concerts</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))}
-                    </div>}
-
-                    {events && <Row xs={1} md={3} className="g-4">
-                        {events.map(event => (
-                            <Col key={event.pk}>
-                                <Card>
-                                    <Card.Img variant="top" src={event.fields.event_image_url} />
-                                    <Card.Body>
-                                        <Card.Title>{event.fields.event_name}</Card.Title>
-                                        <Card.Text>
-                                            <p>Location: {event.fields.event_location}</p>
-                                            <p>Description: {event.fields.description}</p>
-                                            {/* Add more event details as needed */}
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        ))}
-                    </Row>}
+                            ))}
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
--- src/components/search_events/search_events.jsx
+++ src/components/search_events/search_events.jsx
@@ -1,71 +1,80 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../App';
-import { Navbar, Nav, Container, Button, Form } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
 
 const Search = (props) => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [selectedFilter, setSelectedFilter] = useState('');
-  // const [events, setEvents] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [selectedFilter, setSelectedFilter] = useState('');
+    const [eventLocation, setEventLocation] = useState('');
 
-    const searchEvents = async (query) => {
+    const searchEvents = async (query, eventLocation) => {
         try {
-            const response = await fetch(`http://127.0.0.1:8000/events/search_events/?q=${query}`);
+            const response = await fetch(`http://127.0.0.1:8000/events/search_events/?q=${query}&eventLocation=${eventLocation}`);
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
             const data = await response.json();
-            console.log(response);// Parse response body as JSON
-            console.log("Response data:", data); // Log parsed response data
-            props.setEvents(data); // Set parsed JSON data to events state
+            console.log("Response data:", data);
+            props.setEvents(data);
         } catch (error) {
             console.error('There was a problem with your fetch operation:', error);
         }
     };
 
-    const handleSearch = (event, setEvents) => {
+    const handleSearch = (event) => {
         event.preventDefault();
-        console.log(event.target.elements.searchTerm.value); // Check the event target
-        const query = event.target.elements.searchTerm.value;
-        console.log(query); // Check the value of query
-        searchEvents(query, props.setEvents);
+        const query = searchTerm;
+        searchEvents(query, eventLocation);
+        performSearch();
     };
 
+    const performSearch = () => {
+        props.setSearched(true); // Indicate that a search has been made
+    }
 
-  return (
-    <Container className="text-white">
-      <h2 className="mb-4 text-white font-weight-bold">Search Events</h2>
-      <Form onSubmit={handleSearch} className="search-form">
-        <Form.Group controlId="searchTerm" className="mb-3">
-          <Form.Label className="fs-5 font-weight-bold">Search Term</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Search by keywords..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className="bg-transparent text-white search-input"
-          />
-        </Form.Group>
-        <Form.Group controlId="filterSelect" className="mb-4">
-          <Form.Label className="fs-5 font-weight-bold">Filter</Form.Label>
-          <Form.Control
-            as="select"
-            value={selectedFilter}
-            onChange={(e) => setSelectedFilter(e.target.value)}
-            className="bg-transparent text-white filter-select"
-          >
-            <option value="">All</option>
-            <option value="price - low to high">Price - Low to High</option>
-            <option value="time - near to future">Time - Near to Future</option>
-            {/* Add more filter options as needed */}
-          </Form.Control>
-        </Form.Group>
-        <Button variant="primary" type="submit" className="px-4 py-2 search-button">
-          Filter
-        </Button>
-      </Form>
-    </Container>
-  );
+    return (
+        <Container className="text-white">
+            <h2 className="mb-4 text-white font-weight-bold">Search Events</h2>
+            <Form onSubmit={handleSearch} className="search-form">
+                <Form.Group controlId="searchTerm" className="mb-3">
+                    <Form.Label className="fs-5 font-weight-bold">Search Term</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Search by keywords..."
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        className="bg-transparent text-white search-input"
+                    />
+                </Form.Group>
+                <Form.Group controlId="evenLtocation" className="mb-3">
+                    <Form.Label className="fs-5 font-weight-bold">Location</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Filter by eventLocation..."
+                        value={eventLocation}
+                        onChange={(e) => setEventLocation(e.target.value)}
+                        className="bg-transparent text-white search-input"
+                    />
+                </Form.Group>
+                <Form.Group controlId="filterSelect" className="mb-4">
+                    <Form.Label className="fs-5 font-weight-bold">Filter</Form.Label>
+                    <Form.Control
+                        as="select"
+                        value={selectedFilter}
+                        onChange={(e) => setSelectedFilter(e.target.value)}
+                        className="bg-transparent text-white filter-select"
+                    >
+                        <option value="">All</option>
+                        <option value="price - low to high">Price - Low to High</option>
+                        <option value="time - near to future">Time - Near to Future</option>
+                        {/* Add more filter options as needed */}
+                    </Form.Control>
+                </Form.Group>
+                <Button variant="primary" type="submit" className="px-4 py-2 search-button">
+                    Search
+                </Button>
+            </Form>
+        </Container>
+    );
 };
 
 export default Search;
