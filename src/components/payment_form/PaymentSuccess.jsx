6ba8e9e37a693fe58a982081fb2afe62f3c9417d
Jack Ramos <ramos.jack525@yahoo.com>
2024-04-21 19:20:13 -0400

    added back button to payment success page

--- src/components/payment_form/PaymentSuccess.jsx
+++ src/components/payment_form/PaymentSuccess.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import './PaymentSuccess.css';
 function PaymentSuccess() {
@@ -6,9 +7,18 @@ function PaymentSuccess() {
     const payData = location.state;
     console.log(payData)
 
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+        navigate("/dashboard");
+    }
+
     return (
         <div className="payment-success-page">
             <div className="payment-success-content">
+                <button onClick={handleBack} className="btn btn-primary">Back</button>
+                <br/>
+                <br/>
                 <h1>Payment Successful</h1>
                 <div>
                     <p><strong>Event Name:</strong> {payData.event_name}</p>
